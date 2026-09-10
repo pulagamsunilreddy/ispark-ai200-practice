715f75dc490618a3ff74c1e4a964f5691e12e4ee
Sunil Reddy <psunilreddy@hotmail.com>
2026-09-10 20:16:34 +0530

    added dependenty for htmlresponse

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def home():
      return """
     <!DOCTYPE html>
